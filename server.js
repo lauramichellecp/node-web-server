fb74f0821adbb93fe144785b95d4a6b2a48aeae7
lauramichellecp <71450174+lauramichellecp@users.noreply.github.com>
2022-04-05 16:32:16 -0400

    parts 1-4 done

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import helloController from "./controllers/hello-controller.js";
+import userController from "./controllers/user-controller.js";
+
+const app = express();
+
+helloController(app);
+app.use(express.json());
+userController(app);
+
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')});
+app.listen(4000);
