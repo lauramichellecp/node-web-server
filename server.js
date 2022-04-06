765625ee39abb65a841f82c6279f985bde0f56e9
Laura Calderon <calderon.l@northeastern.edu>
2022-04-06 17:55:41 -0400

    finishing up server side of A8

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+
 import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
+import tuitsController from "./controllers/tuits-controller.js";
 
 const app = express();
+app.use(cors());
 
 helloController(app);
 app.use(express.json());
 userController(app);
+tuitsController(app);
 
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')});
 app.listen(4000);
