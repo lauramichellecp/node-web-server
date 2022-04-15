917234a4b2f4648d9c19edb6e098f13151e785e2
Laura Calderon <calderon.l@northeastern.edu>
2022-04-15 18:17:56 -0400

    adding connection string

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -10,25 +10,25 @@ const createTuit = async (req, res) => {
     newTuit.avatarImg = "/tuiter/pic/profile.jpg";
     const insertedTuit = await tuitsDao.createTuit(newTuit);
     res.json(insertedTuit);
-}
+};
 
 const findAllTuits = async (req, res) => {
     const tuits = await tuitsDao.findAllTuits()
     res.json(tuits);
-}
+};
 
 const updateTuit = async (req, res) => {
     const tuitdIdToUpdate = req.params.tid;
     const updatedTuit = req.body;
     const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updatedTuit);
     res.send(200);
-}
+};
 
 const deleteTuit = async (req, res) => {
     const tuitdIdToDelete = req.params.tid;
     const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
     res.send(status);
-}
+};
 
 export default (app) => {
     app.post('/api/tuits', createTuit);
