722c67d04e5b76f4488cc7d03516a82a5514f51f
Laura Calderon <calderon.l@northeastern.edu>
2022-04-15 18:54:33 -0400

    a9 done

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 
 const CONNECTION_STRING = "mongodb+srv://root:revolution9@cluster0.ioavd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    || "mongodb://localhost:27017/webdev"
+    || "mongodb://localhost:27017/webdev";
+
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
