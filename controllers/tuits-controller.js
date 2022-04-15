0c9e67733cfa05c00d084a48f6397723559d3df9
Laura Calderon <calderon.l@northeastern.edu>
2022-04-15 17:56:52 -0400

    changing controllers

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -1,31 +1,33 @@
-import posts from "./tuits/tuits.js";
-let tuits = posts;
+import * as tuitsDao from "../tuits-dao.js";
 
-const createTuit = (req, res) => {
+const createTuit = async (req, res) => {
     const newTuit = req.body;
     newTuit._id = (new Date()).getTime()+'';
     newTuit.likes = 0;
     newTuit.dislikes = 0;
     newTuit.time = "now";
     newTuit.handle = "laura_is_me";
-    newTuit.avatarImg = "/tuiter/pic/profile.jpg"
-    tuits.push(newTuit);
-    res.json(newTuit);
+    newTuit.avatarImg = "/tuiter/pic/profile.jpg";
+    const insertedTuit = await tuitsDao.createTuit(newTuit);
+    res.json(insertedTuit);
 }
 
-const findAllTuits = (req, res) => res.json(tuits);
+const findAllTuits = async (req, res) => {
+    const tuits = await tuitsDao.findAllTuits()
+    res.json(tuits);
+}
 
-const updateTuit = (req, res) => {
+const updateTuit = async (req, res) => {
     const tuitdIdToUpdate = req.params.tid;
     const updatedTuit = req.body;
-    tuits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
-    res.sendStatus(200);
+    const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updatedTuit);
+    res.send(200);
 }
 
-const deleteTuit = (req, res) => {
+const deleteTuit = async (req, res) => {
     const tuitdIdToDelete = req.params.tid;
-    tuits = tuits.filter(t => t._id !== tuitdIdToDelete);
-    res.sendStatus(200);
+    const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
+    res.send(status);
 }
 
 export default (app) => {
